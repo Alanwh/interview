6c9063f8f98035cd3e2f2e7410bdded79862b6ae
hewenjun <hewenjun-jk@360jinrong.net>
2021-03-02 10:48:32 +0800

    perf: 手写vuex

---
+++ vuex/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    {{$store.state.age}}
+    <br>
+    {{$store.getters.addAge}}
+    <br>
+    <button @click="syncClick">同步点击</button>
+    <br>
+    <button @click="asyncClick">异步点击</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods: {
+    syncClick () {
+      this.$store.commit('syncChange', 10)
+    },
+    asyncClick () {
+      this.$store.dispatch('asyncChange', 5)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vuex/src/store/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    age: 10
+  },
+  getters: {
+    addAge (state) {
+      return state.age + 3
+    }
+  },
+  mutations: {
+    syncChange (state, payload) {
+      state.age += payload
+    }
+  },
+  actions: {
+    asyncChange ({ commit }, paload) {
+      setTimeout(() => {
+        commit('syncChange', paload)
+      }, 1000)
+    }
+  },
+  modules: {
+  }
+})
